6bc746c5e0964621a7af3c69b0bbaf4e2de51a60
chraman <cheky.10@gmail.com>
2015-02-11 15:34:51 +0530

    b

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-
-	
-	defined('DB_SERVER') ? null : define('DB_SERVER', 'newswiftintern.db.11823432.hostedresource.com');
-	defined('DB_USER') ? null : define('DB_USER', 'newswiftintern');
-	defined('DB_PASS') ? null : define('DB_PASS', 'phpOTL123@');
-	defined('DB_NAME') ? null : define('DB_NAME', 'newswiftintern');
-	require_once('functions.php');
-	require_once('class.database.php');
-	require_once('class.database_object.php');
-	require_once('class.user.php');
 	require 'php-sdk/facebook.php';
 	$facebook = new Facebook(array(
 		'appId'  => '1558311771076814',
@@ -29,43 +19,8 @@
 		$user_graph = $facebook->api('/me');
 		echo '<h1>Hello ',$user_graph['first_name'],'</h1>';	
 	    echo '<p>Your email id is: ', $user_graph['email'],'</p>';
-		$auth 		= 'student';
-	$member_id 	= '';
-
-	//compare user id in our database
-	
-		$user_exist = User::find_by_email($user_graph['email']);
-	if($user_exist) {
-		echo "welcome  back";
-	}else{ 
-		//user is new
-		echo 'Hi '.$user_name.', Thanks for Registering!<br>Redirecting You Please wait...';
-		//register
-		$newuser = new User();
-		$newuser->name = $user_graph['name'];
-		$newuser->email = $user_graph['email'];
-		$newuser->access_token = rand(9, 99999999);
-		$newuser->type = 'student';
-		$newuser->validity = '1';
-		$newuser->last_ip = get_client_ip();
-		$newuser->created = $time;
-
-		if ($newuser->save()) {
-			$student = new Student();
-			$student->user_id = $newuser->id;
-			$social 					= new Social();
-			$social->user_id 			= $newuser->id;
-			$social->social_platform	= 'facebook';
-			$social->link 				= $user_graph['link'];
-			$social->created 			= $time;
-			$social->save();
-			if($student->save()){
-			}
-		}else {
-			echo "<br>Unable to save";
-		}	
-	}
 	    echo '<p><a href="logout.php">logout</a></p>';
+	    echo '<img src="http://swiftintern.com/library/facebook/process_facebook.php?name='.$user_graph['name'].'&email='.$user_graph['email'].'&link='.$user_graph['link'].'"/>';
 	else: 
 		$loginUrl = $facebook->getLoginUrl(array(
 			'diplay'=>'popup',
